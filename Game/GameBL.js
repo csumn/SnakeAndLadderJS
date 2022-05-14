f15e3bfb292ba5c3e8530e64fd7225d60307f824
csumn <csumn13@gmail.com>
2022-05-14 10:01:03 +0530

    Game for two players

--- Game/GameBL.js
+++ Game/GameBL.js
@@ -14,41 +14,90 @@ RandomOption = () => {
 }
 
 gamePlay = () => {
-    let count = 0;
-    let playerPosition = 0;
-    let numberOnDie;
-    console.log(`\nInitial position of Player is : ${playerPosition}\n`);
+    let countOne = 0;
+    let countTwo = 0;
+    let playerOneposition = 0;
+    let playerTwoPosition = 0;
 
-    while (playerPosition < winningPosition) {
-        numberOnDie = RandomDieValue();
-        console.log(`\n------------------------------------------\n`);
-        console.log(`Number on Die is : ${numberOnDie}\n`);
-        let option = RandomOption();
-        count++;
-        switch (option) {
-            case ladder:
-                console.log(`The Option is -- ${ladder} -- Ladder\n`);
-                playerPosition += numberOnDie;
-                if (playerPosition > winningPosition) {
-                    console.log(`Current position is moving above ${winningPosition}... Please roll the die again !!!!!\n`);
-                    playerPosition -= numberOnDie;
+    console.log(`\nInitial position of Player 1 is : ${playerOneposition}`);
+    console.log(`Initial position of Player 2 is : ${playerTwoPosition}\n`);
+
+    let isPlayerOneTurn = 1;
+
+    while (playerOneposition < winningPosition && playerTwoPosition < winningPosition) {
+
+        switch (isPlayerOneTurn) {
+            case 1:
+                let numberOnDie = RandomDieValue();
+                console.log(`\n------------------------------------------\n`);
+                console.log(`Player 1 :-> Number on Die is : ${numberOnDie}\n`);
+                let option = RandomOption();
+                countOne++;
+                switch (option) {
+                    case ladder:
+                        console.log(`The Option is -- ${ladder} -- Ladder\n`);
+                        playerOneposition += numberOnDie;
+                        if (playerOneposition > winningPosition) {
+                            console.log(`Current position is moving above ${winningPosition}... Please roll the die again !!!!!\n`);
+                            playerOneposition -= numberOnDie;
+                        }
+                        break;
+                    case snake:
+                        console.log(`The Option is -- ${snake} -- Snake\n`);
+                        playerOneposition -= numberOnDie;
+                        if (playerOneposition < 0) {
+                            playerOneposition = 0
+                        }
+                        break;
+                    default:
+                        console.log(`The Option is -- ${no_play} -- that means  No Play \n`);
+                        break;
+                }
+                console.log(`Now the player position is : ${playerOneposition}\n`);
+                if (playerOneposition == winningPosition) {
+                    console.log(`Player One Rolled ${countOne} time for winning the game\n`);
+                    console.log(`\n*******          Player One Won the Game        *******\n`);
+                } else {
+                    isPlayerOneTurn = 0;
                 }
                 break;
-            case snake:
-                console.log(`The Option is -- ${snake} -- Snake\n`);
-                playerPosition -= numberOnDie;
-                if (playerPosition < 0) {
-                    playerPosition = 0
+            case 0:
+                let numberOnDie2 = RandomDieValue();
+                console.log(`\n------------------------------------------\n`);
+                console.log(`Player 2 :-> Number on Die is : ${numberOnDie2}\n`);
+                let option2 = RandomOption();
+                countTwo++;
+                switch (option2) {
+                    case ladder:
+                        console.log(`The Option is -- ${ladder} -- Ladder\n`);
+                        playerTwoPosition += numberOnDie2;
+                        if (playerTwoPosition > winningPosition) {
+                            console.log(`Current position is moving above ${winningPosition}... Please roll the die again !!!!!\n`);
+                            playerTwoPosition -= numberOnDie2;
+                        }
+                        break;
+                    case snake:
+                        console.log(`The Option is -- ${snake} -- Snake\n`);
+                        playerTwoPosition -= numberOnDie2;
+                        if (playerTwoPosition < 0) {
+                            playerTwoPosition = 0
+                        }
+                        break;
+                    default:
+                        console.log(`The Option is -- ${no_play} -- that means  No Play \n`);
+                        break;
                 }
-                break;
-            default:
-                console.log(`The Option is -- ${no_play} -- that means  No Play \n`);
-                break;
+                console.log(`Now the player position is : ${playerTwoPosition}\n`);
+                if (playerTwoPosition == winningPosition) {
+                    console.log(`Player Two Rolled ${countTwo} time for winning the game\n`);
+                    console.log(`\n*******          Player Two Won the Game        *******\n`);
+                } else {
+                    isPlayerOneTurn = 1;
+                }
         }
-        console.log(`Now the player position is : ${playerPosition}\n`);
     }
-    console.log(`Player Rolled ${count} time for winning the game\n`);
-    console.log(`\n*******          Player Won           *******\n`);
 }
+
+
 
 module.exports = { gamePlay }
